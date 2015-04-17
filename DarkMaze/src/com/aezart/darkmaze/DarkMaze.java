0c2251c533938b9b88f6942f0ae6ceeb21583a92
Aezart <Aezart@ENDOS>
2015-04-16 22:58:03 -0700

    Remove unused imports
    Leftovers from moving code over to Scene

--- DarkMaze/src/com/aezart/darkmaze/DarkMaze.java
+++ DarkMaze/src/com/aezart/darkmaze/DarkMaze.java
@@ -1,26 +1,18 @@
 package com.aezart.darkmaze;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Random;
-import java.util.Vector;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 public class DarkMaze extends JFrame{
