00fc843bf3495fd9307b29771ba98b69d0865ff9
Aezart <heroofhyla@gmail.com>
2015-04-18 19:16:38 -0700

    Remove commented code from Screen

--- DarkMaze/src/com/aezart/darkmaze/Screen.java
+++ DarkMaze/src/com/aezart/darkmaze/Screen.java
@@ -26,41 +26,5 @@ public class Screen extends JPanel{
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		darkMaze.currentScene.draw(g);
-		/*drawingG.drawImage(mapImage, 0, 0, null);
-		lightG2D.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
-		lightG2D.fillRect(0, 0, 608, 480);
-		lightG2D.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
-		
-		for (int i = 0; i < darkMaze.coins.length; ++i){
-			for (int k = 0; k < darkMaze.coins[0].length; ++k){
-				if (darkMaze.coins[i][k]){
-					drawingG.drawImage(darkMaze.tileset, k*64+32, i*64+32, k*64+64, i*64+64, 32, 0, 48, 16, null);
-				}
-			}
-		}
-
-		
-		for (Entity e: darkMaze.entities){
-			e.draw(drawingG);
-			e.drawLights(lightG2D);
-		}
-		
-		lightG2D.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_IN));
-		lightG2D.drawImage(drawingSurface, 0, 0, null);
-		finalG.setColor(Color.black);
-		finalG.fillRect(0, 0, 608, 480);
-		finalG.drawImage(lightSurface, 0, 0, null);
-		
-		finalG.setColor(Color.black);
-		finalG.fillRect(0,468,608,480);
-		finalG.setColor(Color.white);
-		finalG.drawString("Lives: " + darkMaze.lives + " Depth: " + (10*darkMaze.level)  + "ft Coins: " + darkMaze.coinCount, 0, 478);
-
-		for (Entity e: darkMaze.entities){
-			e.drawEffects(finalG);
-		}
-		
-		g.drawImage(finalRender, 0, 0, null);		
-		//System.out.println(System.nanoTime());*/
 	}
 }
