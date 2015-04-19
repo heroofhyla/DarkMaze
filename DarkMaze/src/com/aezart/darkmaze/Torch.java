43e5f7c908cccbb343051112857fa4fc46142858
Aezart <heroofhyla@gmail.com>
2015-04-18 19:18:29 -0700

    Fix draw offsets for torch
    Entities should have their sprites centered at their x, y position

--- DarkMaze/src/com/aezart/darkmaze/Torch.java
+++ DarkMaze/src/com/aezart/darkmaze/Torch.java
@@ -3,6 +3,8 @@ package com.aezart.darkmaze;
 public class Torch extends Entity{
 	public Torch(DungeonScene scene){
 		super(scene.game.droppedTorch, scene);
+		drawXOffset = -sprite.getWidth()/2;
+		drawYOffset = -sprite.getHeight()/2;
 	}
 	
 	
